2738f20612809a4504222ca6bbcbcfdc72d26bfd
Jeff Brownlee <jeff.b.brownlee@gmail.com>
2011-03-28 11:00:49 -0500

    Done with Chapter 7

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Jeff Brownlee"
+  user.email                  "jeff.b.brownlee@gmail.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
