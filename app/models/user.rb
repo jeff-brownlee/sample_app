6c4605d5a2eb8c58727718f19ef287fe9e644e72
Jeff Brownlee <jeff.b.brownlee@gmail.com>
2011-03-30 07:43:12 -0500

    Completed Chapter 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110328140819
+# Schema version: 20110330121036
 #
 # Table name: users
 #
@@ -9,6 +9,8 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean
 #
 
 require 'digest'
